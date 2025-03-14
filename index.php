b8c25b13b18c62dbc5f0e1fc5333452481cce9bf
blinkner <gabroc9@gmail.com>
2025-03-14 09:44:26 -0300

    correção de erros e adição de link css

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
-<?php //require_once 'controllers/pessoaController.php'; ?>
+<?php require_once 'controllers/pessoaController.php'; ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Conexão MYSQLI</title>
 </head>
 <body>
@@ -12,11 +13,10 @@
             <h1>Conexão MYSQLI</h1>
         </header>
         <section>
-            <p>A conexão é realizada por meio do arquivo <code>'models/conexao.php'</code></p>
-            <p>A interação com o banco de dados é realizada pelo <code>'models/Pessoa.php'</code></p>
-            <p>A realização de solicitações e intermeio entre o banco de dados e os dados é realizado pelo <code>'controllers/pessoaController.php'</code></p>
+            <p>A conexão é realizada por meio do arquivo <code>'models/conexao.php'</code>.</p>
+            <p>A interação com o banco de dados é realizada pelo <code>'models/Pessoa.php'</code>.</p>
+            <p>A realização de solicitações e intermeio entre o banco de dados e os dados é realizado pelo <code>'controllers/pessoaController.php'</code>.</p>
         </section>
-        <section></section>
     </main>
 </body>
 </html>
